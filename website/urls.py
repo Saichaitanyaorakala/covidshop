f1e9f99014b6da6fd20e2025fd82ef1cb65d786c
Chaitanya <chaitu.orakala@gmail.com>
2020-04-07 01:30:59 -0700

    added profile page

--- website/urls.py
+++ website/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('affiliate.urls')),
+    path('sai/', include('profile1.urls')),
 ]
